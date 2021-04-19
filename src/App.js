78d5fdc50d69124286f49550b79b64c68ffc2c71
Ricardo Lugo <ricardo.lugo@nuvem.mx>
2021-04-18 19:45:46 -0500

    develop | Utilizando los hooks de estado

--- src/App.js
+++ src/App.js
@@ -1,33 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
 //Se exporta entre llaves el componente que solo tiene export
 /*
 import HolaMundo {AdiosMundo} from "./components/HolaMundo";
-import AdiosMundo from './components/AdiosMundo'
+import AdiosMundo from './components/AdiosMundo';
+import Saludar from './components/Saludar';
 */
-import Saludar from './components/Saludar'
 
 function App() {
 
-  const user = {
-    nombre: "Ricardo Omar",
-    edad: 25,
-    color: "Azul"
-  }
+  const [stateCar, setStateCar] = useState(false);
 
-  const saludarFn = (nombre, edad) =>{
-    console.log("Hola "+ nombre + " tiene " + edad + " años");
-
-    console.log(`Hola ${nombre}, tiene ${edad} años.`); //Template String
+  const encenderApagar = () => {
+    //setStateCar(!stateCar);
+    setStateCar(prevValue => !prevValue); //Obteniendo el valor previo de setStateCar cuando solo se tiene acceso a la funcion setStateCar
   }
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -36,7 +29,8 @@ function App() {
         >
           Learn React
         </a>
-        <Saludar userInfo={user} saludarFn={saludarFn}/>
+        <h3>El coche esta: { stateCar ? "Encendido" : "Apagado" }</h3>
+        <button onClick={encenderApagar}> Encender / Apagar</button>
       </header>
       
     </div>
